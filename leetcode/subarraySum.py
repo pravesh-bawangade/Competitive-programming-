c3e89970f9875594c050472dee2f1cd685060a27
Pravesh Bawangade <pptechno52@gmail.com>
2020-05-06 23:07:55 +0530

    added file in leetcode

--- leetcode/subarraySum.py
+++ leetcode/subarraySum.py
@@ -3,7 +3,6 @@ from typing import List
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         sum = {0:1}
-        n = len(nums)
         count = 0
         s = 0
         for num in nums:
