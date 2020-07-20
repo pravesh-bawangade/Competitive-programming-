04ef2d03e2d1d874b854844fad1fb1a6ee6cea15
Pravesh Bawangade <pptechno52@gmail.com>
2020-07-20 23:09:39 +0530

    updated file

--- Assignment-icapotech/chpintu.c
+++ Assignment-icapotech/chpintu.c
@@ -1,19 +1,15 @@
-/*
-File_name : chpintu.c
-Author : Pravesh Bawangade
-*/
+#include <stdio.h>
 
-#include<stdio.h>
-
-int main(){
+int main(void) {
+    // inputs
     int T;
-    scanf("%d", &T);
-    while(T--){
-        // take inputs
-        int N, M;
-        scanf("%d", &N);
+	scanf("%d",&T);
+	while(T--)
+	{
+	    int N, M;
+	    scanf("%d", &N);
         scanf("%d", &M);
-        int N_array[N];
+	    int N_array[N];
         int P_array[N];
         for(int i=0;i<N;i++){
             scanf("%d", &N_array[i]);
@@ -21,23 +17,30 @@ int main(){
         for(int i=0;i<N;i++){
             scanf("%d", &P_array[i]);
         }
-
-        // main calculations 
-        int min = 1000000;
-        int sum = 0;
-        for(int i=1; i<=M; i++){
-            for(int j=0; j<N; j++){
-                if(N_array[j]==i){
-                    sum = sum + P_array[j];
-                }
-            }
-            if(sum < min){
-                min = sum;
-                sum = 0;
-            }    
-        }
-        printf("%d\n", min);    
-        
-    }
-    return 0;
+        int arr[M];
+	    for(int i=0; i<M; i++)
+		{
+	        arr[i]=-1;
+	    }
+	    
+	    // main calculation
+	    for(int i=0; i<N; i++)
+	    {
+	        if(arr[N_array[i]-1]==-1)
+	        arr[N_array[i]-1]=0;
+	        
+	        arr[N_array[i]-1]+=P_array[i];
+	    }
+	    int min=10000;
+	    for(int i=0; i<M; i++)
+	    {
+	        if(arr[i]!=-1)
+	        {
+	            if(arr[i]<min)
+	            min=arr[i];
+	        }
+	    }
+        printf("%d\n", min);
+	}
+	return 0;
 }
